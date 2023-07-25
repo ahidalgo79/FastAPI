ac6ac65905ee7ba3c5a29f0579355788b53aeb72
ahidalgo79 <137845402+ahidalgo79@users.noreply.github.com>
2023-07-25 12:59:54 -0600

    Update s04_13_pydantic.py

--- S04_types/s04_13_pydantic.py
+++ S04_types/s04_13_pydantic.py
@@ -1,17 +1,55 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from datetime import datetime
+
+from pydantic import BaseModel, PositiveInt
 
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+class User(BaseModel):
+    id: int  
 
 
-app = FastAPI()
+    name: str = 'John Doe'  
 
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+    signup_ts: datetime | None  
+
+
+    tastes: dict[str, PositiveInt]  
+
+
+
+
+external_data = {
+    'id': 123,
+    'signup_ts': '2019-06-01 12:22',  
+
+
+    'tastes': {
+        'wine': 9,
+        b'cheese': 7,  
+
+
+        'cabbage': '1',  
+
+
+    },
+}
+
+user = User(**external_data)  
+
+
+
+print(user.id)  
+
+
+#> 123
+print(user.model_dump())  
+
+
+"""
+{
+    'id': 123,
+    'name': 'John Doe',
+    'signup_ts': datetime.datetime(2019, 6, 1, 12, 22),
+    'tastes': {'wine': 9, 'cheese': 7, 'cabbage': 1},
+}
+"""
